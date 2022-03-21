89ae895a7a237170f128a54b51bbb4c68d169e9b
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2022-03-21 15:32:30 +0900

    use bakeware instead of escript

--- mix.exs
+++ mix.exs
@@ -1,37 +1,46 @@
 defmodule Tdig.MixProject do
   use Mix.Project
 
+  @app :tdig
+
   def project do
     [
       app: :tdig,
-      escript: escript_config(),
-      version: "0.2.0",
+      version: "0.3.0",
       name: "Tdig",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [{@app, release()}],
+      preferred_cli_env: [release: :prod]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Tdig.CLI, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:bakeware, "~> 0.2.3", runtime: false},
       {:tenbin_dns, git: "https://github.com/toshi0806/tenbin_dns.git", tag: "0.3.0"},
       {:socket, "~> 0.3.13"},
       {:zoneinfo, "~> 0.1.0"},
     ]
   end
 
-  defp escript_config do
+  defp release do
     [
-      main_module: Tdig.CLI
+      overwrite: true,
+      cookie: "#{@app}_cookie",
+      quiet: true,
+      steps: [:assemble, &Bakeware.assemble/1],
+      strip_beams: Mix.env() == :prod
     ]
   end
 end
